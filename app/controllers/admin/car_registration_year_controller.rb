999bde8e8b619ec470773651295c0343498d2d31
Subhamoy Das <subhomay.das@kreeti.com>
2021-09-29 02:49:41 +0530

    init commit

--- app/controllers/admin/car_registration_year_controller.rb
+++ app/controllers/admin/car_registration_year_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CarRegistrationYearController < ApplicationController
 
   before_action :authorize_admin
-  
+
   def new
     @car_registration_year_range = CarRegistrationYear.new
   end
@@ -12,27 +12,26 @@ class Admin::CarRegistrationYearController < ApplicationController
     car_registration_year[:range2] = params[:range2]
     @car_registration_year = CarRegistrationYear.new(car_registration_year)
     if @car_registration_year.save
-        redirect_to admin_car_registration_year_index_path, flash: {notice: "Successfully updated"}
+      redirect_to admin_car_registration_year_index_path, flash: {notice: "Successfully updated"}
     else
-        redirect_to new_admin_car_registration_year_path,  flash: {notice: "an error occured"}
+      redirect_to new_admin_car_registration_year_path,  flash: {notice: "an error occured"}
     end
   end
 
-  def edit 
+  def edit
     @car_registration_year = CarRegistrationYear.find(params[:id])
   end
 
   def update
     p = CarRegistrationYear.where(id: params[:id]).update(range1: params[:range1], range2: params[:range2])
-    if p 
-        redirect_to admin_car_registration_year_index_path, flash: {notice: "Successfully updated"}
-    else 
-        redirect_to admin_car_registration_year_index_path, flash: {error: 'an error occured' }
-  
+    if p
+      redirect_to admin_car_registration_year_index_path, flash: {notice: "Successfully updated"}
+    else
+      redirect_to admin_car_registration_year_index_path, flash: {error: 'an error occured' }
     end
   end
 
   def index
-    @car_registration_years = CarRegistrationYear.all 
+    @car_registration_years = CarRegistrationYear.all
   end
 end
