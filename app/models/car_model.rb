a7ee1aa465f0aa627f6a195f6f462388ddca9ab7
Subhamoy Das <subuamoydas24@gmail.com>
2021-10-27 16:38:42 +0530

    27-oct-21

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -2,7 +2,7 @@ class CarModel < ApplicationRecord
   belongs_to :brand
   validates :name, allow_blank: false, presence: true
 
-  scope :get_car_models_by_brand, ->(params) { where(:brand_id => params[:brand_id]) }
+  scope :get_car_models_by_brand, ->(params) { where(brand_id: params[:brand_id]) }
   scope :car_model_map, -> { all.pluck(:name, :id) }
 end
 
